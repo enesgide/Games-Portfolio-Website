205535e9001b71d6c38a82659adc536521218a62
Enes <mgid9133@uni.sydney.edu.au>
2024-04-02 22:05:33 +1100

    header layout

---
+++ src/components/header.jsx
@@ -0,0 +1,40 @@
+import {Link} from "react-router-dom"
+
+const Header = () => {
+    const navLinks = [
+        {id: 1, text: "Home", url: "/"},
+        {id: 2, text: "Games", url: "/games"},
+        {id: 3, text: "Blogs", url: "/blogs"},
+        {id: 4, text: "Merch", url: "/merch"},
+    ]
+
+    return (
+        <header>
+            <div className="nav">
+                <img style={{marginRight: "20px"}} src="logo.png" alt="logo" height="50"/>
+                <div className="nav-content">                    
+                    <ul className="nav-buttons">
+                        { navLinks.map(navLink => {
+                            return <NavButton key={navLink.id} text={navLink.text} url={navLink.url}></NavButton>
+                        }) }
+                    </ul>       
+                    <div className="contact-nav-button">
+                        <button>CONTACT US</button>
+                    </div>         
+                </div>
+            </div>            
+        </header>
+    );
+};
+
+const NavButton = (props) => {
+    return (
+        <Link to={props.url}>
+            <li className="nav-text">
+                {props.text}
+            </li>
+        </Link>
+    );
+}
+
+export default Header;
---
+++ src/views/home.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/header.jsx'
+
+const Home = () => {
+    return (
+        <main>
+            <Header />
+            <h1>Home page</h1>
+        </main>
+    );
+};
+
+export default Home;
